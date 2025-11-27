53beb7802beda5206437c18d52d0286b35ec4b39
mxdeyli <natsumetooru13@gmail.com>
2025-11-27 20:36:37 +0800

    轮播模式也已完成，只剩下按钮切换功能未完成

--- onlinestudy/js/goods.js
+++ onlinestudy/js/goods.js
@@ -1,44 +1,21 @@
-windows.onload=function()
+window.onload=function()
 {
     function hotChange(){
 		var current_index=0;
-		var timer=window.setInterval(autoChange, 3000);
-		var button_li=document.getElementById("button").getElementsByTagName("li");
+		window.setInterval(autoChange,3000);
 		var pic_li=document.getElementById("pic").getElementsByTagName("li");
-		for(var i=0;i<button_li.length;i++){
-			button_li[i].onmouseover=function(){
-				if(timer){
-					clearInterval(timer);
-				}
-				for(var j=0;j<pic_li.length;j++){
-					if(button_li[j]==this){
-						current_index=j;
-						button_li[j].className="sp";
-						pic_li[j].className="sp";
-					}else{
-						pic_li[j].className="ic";
-						button_li[j].className="back";
-					}
-				}
-			}
-			button_li[i].onmouseout=function(){
-				timer=setInterval(autoChange,3000);			
-			}
-		}
-		function autoChange(){
+		function autoChange()
+		{
 			++current_index;
-			if (current_index==button_li.length) {
+			if(current_index==pic_li.length)
+			{
 				current_index=0;
 			}
-			for(var i=0;i<button_li.length;i++){
-				if(i==current_index){
-					button_li[i].className="sp";
-					pic_li[i].className="sp";
-				}else{
-					button_li[i].className="next";
-					pic_li[i].className="ic";
-				}
+			for(var i=0;i<pic_li.length;i++)
+			{
+				pic_li[i].className="ic";
 			}
+			pic_li[current_index].className="sp";
 		}
 	}
 	hotChange();
